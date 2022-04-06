902a80c4e8bee0d07a38d57e7f933fb07b4e3ea5
mkcarl <munkye29@gmail.com>
2022-04-06 16:47:56 +0800

    removed _middleware.ts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,20 @@
 import type { NextPage } from 'next'
+import { useRouter } from 'next/router';
 import Layout from "./Layout";
+import {useEffect} from "react";
 
 const Home: NextPage = () => {
+    const router = useRouter()
+
+    useEffect(()=>{
+        const {pathname} = router
+        if (pathname == '/')
+            router.push('/home')
+    }, [])
+
   return (
     <Layout title={"Hello world test"}>
+        {router.push('/home')}
         <h1>Hello world</h1>
         <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusamus at atque commodi corporis dicta eius eligendi eum in velit. Aliquid architecto dicta dolor eos, eveniet quaerat quas rerum tempora.
