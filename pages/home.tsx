5a0992a4f7a8e1edbb2b16884f51797b224f71a9
mkcarl <munkye29@gmail.com>
2022-04-06 23:40:48 +0800

    added margin to homepage

--- pages/home.tsx
+++ pages/home.tsx
@@ -15,15 +15,17 @@ const Home : NextPage = () => {
         <Layout title={"Home"}>
             <Box
             sx={{
-                height:'90vh',
+                minHeight:'90vh',
                 display:'flex',
                 flexDirection:'column',
                 textAlign:'center',
                 justifyContent:'center',
-                alignItems:'center'
+                alignItems:'center',
             }}
+            mx={'2rem'}
+            my={'2rem'}
             >
-                <Typography variant={'h1'} >Carl Yan</Typography>
+                <Typography variant={'h1'} noWrap>Carl Yan</Typography>
                 <Typography variant={'h5'}>
                     Hi there, I&apos;m Carl from Malaysia. Currently pursuing my bachelor&apos;s degree in the field of Computer Science (Artificial Intelligence). Welcome to my website where I showcase my past programming projects for my university work, as well as my personal hobby projects. Hope you get to understand more about my projects. Feel free to contact me through the contacts available in my <Link onClick={gotoProfilePage}>profile</Link> page.
                 </Typography>
